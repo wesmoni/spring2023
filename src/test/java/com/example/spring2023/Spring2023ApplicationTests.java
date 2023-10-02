d335d45a8de9f8796aeea30c4b0c7adf28d21afd
naorico <laersv@mail.ru>
2023-10-02 22:09:47 +0500

    Импортировала archunit и добавила тесты

--- src/test/java/com/example/spring2023/Spring2023ApplicationTests.java
+++ src/test/java/com/example/spring2023/Spring2023ApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.spring2023;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @SpringBootTest
 class Spring2023ApplicationTests {
 
+    private static final String PACKAGE_NAME = "com.example.spring2023";
+    private static final JavaClasses CLASSES = new ClassFileImporter().importPackages(PACKAGE_NAME);
     @Test
-    void contextLoads() {
+    @DisplayName("Check package app")
+    void testAppPackage() {
+        layeredArchitecture()
+                .consideringOnlyDependenciesInLayers()
+                .layer("app").definedBy(PACKAGE_NAME + ".app")
+                .check(CLASSES);
     }
+
+    @Test
+    @DisplayName("Check package domain")
+    void testDomainPackage() {
+        layeredArchitecture()
+                .consideringOnlyDependenciesInLayers()
+                .layer("domain").definedBy(PACKAGE_NAME + ".domain")
+                .check(CLASSES);
+    }
+
+    @Test
+    @DisplayName("Check package extern")
+    void testExternPackage() {
+        layeredArchitecture()
+                .consideringOnlyDependenciesInLayers()
+                .layer("extern").definedBy(PACKAGE_NAME + ".extern")
+                .check(CLASSES);
+    }
+
+    @Test
+    @DisplayName("Check package controller")
+    void testControllerPackage() {
+        layeredArchitecture()
+                .consideringOnlyDependenciesInLayers()
+                .layer("controller").definedBy(PACKAGE_NAME + ".controller")
+                .check(CLASSES);
+    }
+
 
 }
